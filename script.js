ca2172b9b790179ed0fe431a29cf6d3544071ddd
SalustianoJunior <152342678+SalustianoJunior@users.noreply.github.com>
2024-01-29 10:58:02 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,26 @@
+function calcular() {
+    let heroi = document.getElementById('heroi').value;
+    let xp = Number(document.getElementById('xp').value);
+    let nivel = "";
+    let resultado = document.querySelector('div#resultado');
+    
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    resultado.innerHTML = `Seu heroi ${heroi} é level ${nivel}`;
+}
